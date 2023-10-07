7c6980cdf01bee34f9c1761a1f4e8a34089330eb
qiny <qinyuan@zjugis.com>
2023-10-07 16:44:25 +0800

    fix: 搭好台子

--- src/pages/ComponentUse/index.tsx
+++ src/pages/ComponentUse/index.tsx
@@ -1,8 +1,10 @@
 /**
  * @description 组件的使用案例
  */
+import { FC, useRef, useState, useEffect } from "react";
 import { CaseComponent } from "@/components";
-function ComponentUse() {
+import "./style.scss";
+const ComponentUse: FC = () => {
   const codeExample = `
   // 一个暂时代码的组件
   // 可以用于展示示例代码
@@ -13,6 +15,6 @@ function ComponentUse() {
       <CaseComponent code={codeExample} width="600px" />
     </>
   );
-}
+};
 
 export default ComponentUse;
